855d2f8fca01020933266f4c1f1a5108c5b88250
Amit Chavda <amitc@monovative.com>
2022-01-11 19:30:52 +0530

    still getting singlwe page data

--- Javascripts/Js1.js
+++ Javascripts/Js1.js
@@ -1,41 +1,41 @@
-function() {
-
+function(prevData) {
+    alert(prevData)
     let titles = "";
     let links = "";
+    if (prevData == null) {
+        prevData = "~";
+    } else {
+        titles = prevData.split("~")[0];
+        links = prevData.split("~")[1];
+    }
 
-    let nPgaes = getTotalPage();
 
-    for (let x = 0; x < nPgaes; x++) {
+    for (let i = 1; i < document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children.length; i++) {
 
-        for (let i = 1; i < document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children.length; i++) {
+        var val = checkRegex(document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children[i].innerText);
 
-            var val = checkRegex(document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children[i].innerText);
+        if (val) {
+            var link = document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children[i].getElementsByClassName("a-link-normal s-link-style a-text-normal")[0];
+            if (link != undefined) {
+                title = link.innerText;
 
-            if (val) {
-                var link = document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children[i].getElementsByClassName("a-link-normal s-link-style a-text-normal")[0];
-                if (link != undefined) {
-                    title = link.innerText;
-
-                    links += link + "#";
-                    titles += title + "=";
-                }
+                links += link + "#";
+                titles += title + "=";
             }
         }
-        setTimeout(function() {}, 2000);
-        clickBtn();
     }
 
     return titles + "~" + links;
 }
 
-function clickBtn() {
-    document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li")[document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li").length - 1].children[0].click();
-}
+// function clickBtn() {
+//     document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li")[document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li").length - 1].children[0].click();
+// }
 
-function getTotalPage() {
-    var len = document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li")[document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li").length - 2].innerText;
-    return len;
-}
+// function getTotalPage() {
+//     var len = document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li")[document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li").length - 2].innerText;
+//     return len;
+// }
 
 function checkRegex(str) {
     var words = ["Delivery", "Get it by", "stock", "out of", "starts"];
