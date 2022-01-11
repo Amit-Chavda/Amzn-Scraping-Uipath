1188e1dc6b6270cb665303dba9814b27c434ec3e
Amit Chavda <amitc@monovative.com>
2022-01-11 17:26:58 +0530

    JS code changed for muliple changes and still working on

--- Javascripts/Js1.js
+++ Javascripts/Js1.js
@@ -2,21 +2,39 @@ function() {
 
     let titles = "";
     let links = "";
-    for (let i = 1; i < document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children.length; i++) {
 
-        var val = checkRegex(document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children[i].innerText);
+    let nPgaes = getTotalPage();
 
-        if (val) {
-            var link = document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children[i].getElementsByClassName("a-link-normal s-link-style a-text-normal")[0];
-            if (link != undefined) {
-                title = link.innerText;
+    for (let x = 0; x < nPgaes; x++) {
 
-                links += link + "#";
-                titles += title + "=";
+        for (let i = 1; i < document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children.length; i++) {
+
+            var val = checkRegex(document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children[i].innerText);
+
+            if (val) {
+                var link = document.getElementsByClassName("s-main-slot s-result-list s-search-results sg-row")[0].children[i].getElementsByClassName("a-link-normal s-link-style a-text-normal")[0];
+                if (link != undefined) {
+                    title = link.innerText;
+
+                    links += link + "#";
+                    titles += title + "=";
+                }
             }
         }
+        setTimeout(function() {}, 2000);
+        clickBtn();
     }
+
     return titles + "~" + links;
+}
+
+function clickBtn() {
+    document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li")[document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li").length - 1].children[0].click();
+}
+
+function getTotalPage() {
+    var len = document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li")[document.getElementsByClassName("celwidget slot=MAIN template=PAGINATION widgetId=pagination-button")[0].children[0].querySelectorAll("li").length - 2].innerText;
+    return len;
 }
 
 function checkRegex(str) {
